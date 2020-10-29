73e7fe359fcb070b962efac2b32b850c47b76718
dipto <Sm Shihubullah Dipto>
2020-10-29 18:13:50 -0400

    setting up tests

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# test that xxs is fixed
+class XXSTestCase(TestCase):
+	fixtures = ['testdata.json']
+	def setUp(self):
+		
+	def testXXS(self):
+
+# test that csrf is fixed
+class CSRFTestCase(TestCase):
+	fixtures = ['testdata.json']
+	def setUp(self):
+		
+	def testCSRF(self):
+
+# test that sqli is fixed
+class SQLITestCase(TestCase):
+	fixtures = ['testdata.json']
+	def setUp(self):
+		
+	def testSQLI(self):
+
+# test that salts for passwords are different
+class SALTTestCase(TestCase):
+	fixtures = ['testdata.json']
+	def setUp(self):
+		
+	def testSALT(self):
