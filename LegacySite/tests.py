29c1f91e0c6b29736495eb371bf4882eb7de8a5e
dipto <Sm Shihubullah Dipto>
2020-10-30 11:12:53 -0400

    fixed xxs

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 
@@ -6,26 +6,42 @@ from django.test import TestCase
 class XXSTestCase(TestCase):
 	fixtures = ['testdata.json']
 	def setUp(self):
+		self.client = Client()
 		
 	def testXXS(self):
+		response = self.client.get('/buy')
+		self.assertEqual(response.status_code, 200)
+		bad_script = '<p>Endorsed by <script>alert("hello")</script>!</p>'
+		response = self.client.get('/buy.html?director=''<script>alert("hello")</script>''')
+		#print(response.content)
+		if bad_script in (response.content).decode():
+			print("xxs attack works\n")
+		else:
+			print("xxs attack failed\n")
 
 # test that csrf is fixed
 class CSRFTestCase(TestCase):
 	fixtures = ['testdata.json']
 	def setUp(self):
+		self.client = Client()
 		
 	def testCSRF(self):
+		print()
 
 # test that sqli is fixed
 class SQLITestCase(TestCase):
 	fixtures = ['testdata.json']
 	def setUp(self):
-		
+		self.client = Client()
+	
 	def testSQLI(self):
+		print()
 
 # test that salts for passwords are different
 class SALTTestCase(TestCase):
 	fixtures = ['testdata.json']
 	def setUp(self):
+		self.client = Client()
 		
 	def testSALT(self):
+		print()
