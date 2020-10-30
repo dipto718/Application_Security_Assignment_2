c95f96022a8a2f747a5be2222812a35be8ab0a45
dipto <Sm Shihubullah Dipto>
2020-10-30 16:21:11 -0400

    fixes and tests done

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from . import extras
 
 # Create your tests here.
 
@@ -32,10 +33,10 @@ class CSRFTestCase(TestCase):
 		# when the attacker try to make them
 		# buy a giftcard
 		self.client = Client()
-		response = self.client.post('/login', {'uname' : 'z', 'pword' : 'z'})
+		response = self.client.post('/login', {'uname' : 'abc123', 'pword' : 'abc123'})
 		#print(response.status_code)
 		if response.status_code == 200:
-			print("user z logged in ")
+			print("user abc123 logged in ")
 		# by default the test client disables any
 		# any CSRF checks in the setting for the
 		# website so this reenables them
@@ -54,17 +55,19 @@ class SQLITestCase(TestCase):
 	fixtures = ['testdata.json']
 	def setUp(self):
 		self.client = Client()
+		#response = self.client.post('/login', {'uname' : 'abc123', 'pword' : 'abc123'})
 	
 	def testSQLI(self):
 		#with open('newcard.gftcrd') as card:
 		#	card_data = card.read()
-		card = open('newcard.gftcrd')
-		response = self.client.post('/use', {'card_data' : card, 'card_supplied' : 'True', 'card_fname' : 'card'})
-		admin_pword_hashed = "000000000000000000000000000078d2$18821d89de11ab18488fdc0a01f1ddf4d290e198b0f80cd4974fc031dc2615a3"
-		#print(response.status_code)
-		if admin_pword_hashed in response.content.decode():
-			print("can still get admin hashed password so sqli attack still works\n")
-		else:
+		try:
+			card = open('newcard.gftcrd')
+			response = self.client.post('/use', {'card_data' : card, 'card_supplied' : 'True', 'card_fname' : 'card'})
+			admin_pword_hashed = "000000000000000000000000000078d2$18821d89de11ab18488fdc0a01f1ddf4d290e198b0f80cd4974fc031dc2615a3"
+			#print(response.status_code)
+			if admin_pword_hashed in response.content.decode():
+				print("can still get admin hashed password so sqli attack still works\n")
+		except:
 			print("can't get admin hashed password so sqli attack fails\n")
 		#print()
 
@@ -75,4 +78,7 @@ class SALTTestCase(TestCase):
 		self.client = Client()
 		
 	def testSALT(self):
-		print()
+		if extras.generate_salt(16) == extras.generate_salt(16):
+			print("The salt is still weak since it is always the same\n")
+		else:
+			print("The salt is strong since it is now not always the same\n")
